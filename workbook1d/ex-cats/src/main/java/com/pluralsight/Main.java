35a07d1aec0d5bc25ad2c5ca0d34f6e3176a09b1
Paul Hannon <phannon527@gmail.com>
2025-09-24 15:39:13 -0700

    Created StringApplication project

--- workbook1d/ex-cats/src/main/java/com/pluralsight/Main.java
+++ workbook1d/ex-cats/src/main/java/com/pluralsight/Main.java
@@ -6,6 +6,30 @@ import java.util.Arrays;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Sout is sick!");
-    }
-}
+        //Primitive variables
+        String favColor = "Black";
+        String favArtist = "Daft Punk";
+        boolean didEat = false;
+        int age = 21;
+        String firstName = "Paul";
+        String lastName = "Hannon";
+        lastName = "Dinoguy";
+        String newFullname = firstName + " " + lastName;
+        System.out.println(favColor);
+        System.out.println(favArtist);
+        System.out.println(didEat);
+        System.out.println(age);
+        System.out.println(firstName);
+        System.out.println(lastName);
+
+        boolean isHappy = true;
+        boolean eatenBreakfast = false;
+
+        if (isHappy && eatenBreakfast) {
+            System.out.println("Nice job!");
+        } else if (isHappy && !eatenBreakfast) {
+            System.out.println("You're happy but hungry");
+        } else if (!isHappy && eatenBreakfast) {
+            System.out.println("Do something about it");
+        }
+    }}
