7519bf1501ca5858eaa4a9c2461ed09498d4ea8f
Paul Hannon <phannon527@gmail.com>
2025-09-25 09:12:25 -0700

    Created movie list

---
+++ workbook1d/RecapFunDay4/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        String movieName = "Jumper";
+        int numberOfFriends = 7;
+        double pizzaPrice = 4.99;
+        boolean havePopcorn = true;
+
+        System.out.println("Invite " + numberOfFriends + " friends over.");
+        System.out.println("Order pizza for the price of $" + pizzaPrice + ".");
+        System.out.println("Popcorn?" + havePopcorn);
+        System.out.println("Pick and watch the movie " + movieName + ".");
+
+        long rounded = Math.round(pizzaPrice);
+        System.out.println("Rounded value: " + rounded );
+
+        int newPizzaPrice = 5;
+        boolean overPriced = false;
+
+        if (overPriced)
+            System.out.println("Whoa, that's expensive pizza!");
+        else
+            System.out.println("Nice budget-friendly movie night!");
+
+
+
+    }
+}
