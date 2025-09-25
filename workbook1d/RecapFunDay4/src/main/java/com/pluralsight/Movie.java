3426974bfa67297d3ebe35c6f68bcb940002c556
Paul Hannon <phannon527@gmail.com>
2025-09-25 09:18:05 -0700

    Created movie list

--- workbook1d/RecapFunDay4/src/main/java/com/pluralsight/Movie.java
+++ workbook1d/RecapFunDay4/src/main/java/com/pluralsight/Movie.java
@@ -9,10 +9,21 @@ public class Movie {
         double pizzaPrice = 4.99;
         boolean havePopcorn = true;
 
-        System.out.println("Invite " + numberOfFriends + " over.");
+        System.out.println("Invite " + numberOfFriends + " friends over.");
         System.out.println("Order pizza for the price of $" + pizzaPrice + ".");
         System.out.println("Popcorn?" + havePopcorn);
         System.out.println("Pick and watch the movie " + movieName + ".");
+
+        long rounded = Math.round(pizzaPrice);
+        System.out.println("Rounded value: " + rounded );
+
+        int newPizzaPrice = 5;
+        boolean overPriced = false;
+
+        if (overPriced)
+            System.out.println("Whoa, that's expensive pizza!");
+        else
+            System.out.println("Nice budget-friendly movie night!");
     }
 
 }
