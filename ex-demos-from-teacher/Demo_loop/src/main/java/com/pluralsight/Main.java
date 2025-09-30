5b97cb7689cb2725c76a45d687e681117e604bcf
Paul Hannon <phannon527@gmail.com>
2025-09-30 09:45:31 -0700

    Created project for loops

--- ex-demos-from-teacher/Demo_loop/src/main/java/com/pluralsight/Main.java
+++ ex-demos-from-teacher/Demo_loop/src/main/java/com/pluralsight/Main.java
@@ -1,5 +1,7 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -14,6 +16,7 @@ public class Main {
         for (int i = 81; i > 1; i--) {
             System.out.printf("%d days left for year up academy.%n",i);
         }
+
         for (int i = 2; i <= 10; i+=2) {
             System.out.println(i);
         }
@@ -24,5 +27,20 @@ public class Main {
             System.out.println("Countdown: " + j);
             j--;
         }
+
+        Scanner scanner = new Scanner(System.in);
+
+        int nextYear = 2026;
+        System.out.println("What year will next year be?");
+        nextYear = scanner.nextInt();
+
+        while (nextYear < 2026 || nextYear > 2026) {
+            System.out.println("It cannot be that year.");
+            System.out.print("What year will next year be?");
+            nextYear = scanner.nextInt();
+        }
+        System.out.println("You are correct.");
+        scanner.close();
+        }
+
     }
-}
