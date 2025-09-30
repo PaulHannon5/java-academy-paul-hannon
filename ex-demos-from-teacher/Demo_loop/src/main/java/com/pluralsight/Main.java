88ef90cf009af807364adb279a8c1777e12dc578
Paul Hannon <phannon527@gmail.com>
2025-09-30 09:00:51 -0700

    Created for loops

---
+++ ex-demos-from-teacher/Demo_loop/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("I will be a good developer!");
+
+        }
+        for (int i = 0; i < 81; i++) {
+            System.out.println("I have 81 days left of the year up academy.");
+        }
+        for (int i = 81; i > 1; i--) {
+            System.out.printf("%d days left for year up academy.%n",i);
+        }
+    }
+}
